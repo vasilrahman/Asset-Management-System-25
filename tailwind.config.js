d637bfa8cf6518f7b22f79a5e739e1b3a8428af3
Anand Venugopalan <anandvenugopalan167@gmail.com>
2026-01-23 14:52:56 +0530

    feat: Tailwind CDN issue

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        slate: {
+          750: '#2d3748',
+          850: '#1a202c',
+          950: '#020617',
+        },
+        primary: {
+          50: '#eef2ff',
+          100: '#e0e7ff',
+          500: '#6366f1',
+          600: '#4f46e5',
+          700: '#4338ca',
+          900: '#312e81',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
